bf707972b7902d7bbaf6588aa97a7210c6a2a37e
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-05-23 18:10:19 +0200

    Updater: Initialize ProjectStreak

--- src/org/lineageos/updater/misc/Constants.java
+++ src/org/lineageos/updater/misc/Constants.java
@@ -39,11 +39,11 @@ public final class Constants {
 
     public static final String PROP_AB_DEVICE = "ro.build.ab_update";
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
-    public static final String PROP_BUILD_VERSION = "ro.lineage.build.version";
-    public static final String PROP_DEVICE = "ro.lineage.device";
+    public static final String PROP_BUILD_VERSION = "ro.streak.build.version";
+    public static final String PROP_DEVICE = "ro.streak.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
-    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "lineage.updater.allow_downgrading";
-    public static final String PROP_UPDATER_URI = "lineage.updater.uri";
+    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "streak.updater.allow_downgrading";
+    public static final String PROP_UPDATER_URI = "streak.updater.uri";
     public static final String PROP_RECOVERY_UPDATE = "persist.sys.recovery_update";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
